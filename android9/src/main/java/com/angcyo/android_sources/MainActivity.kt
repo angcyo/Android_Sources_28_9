6dd17f2c7b394abc0c55d0b8d1a571937af11f1a
angcyo <angcyo@126.com>
2019-01-12 00:19:30 +0800

    init

---
+++ android9/src/main/java/com/angcyo/android_sources/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.angcyo.android_sources
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
